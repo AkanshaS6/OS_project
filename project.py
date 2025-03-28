ae12659f95113b1233bff52f64bdbdf95e661e73
AkanshaS6 <149610478+AkanshaS6@users.noreply.github.com>
2025-03-28 15:20:40 +0530

    Update project.py

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import numpy as np
 def fcfs(requests, head):
     """ First-Come-First-Serve Disk Scheduling """
     seek_sequence = [head] + requests
-    seek_time = sum(abs(seek_sequence[i] - seek_sequence[i+1]) for i in range(len(seek_sequence) - 1))
+    seek_time = sum(abs(seek_sequence[i] - seek_sequence[i+1]) for i in range(len(seek_sequence) -1))
     return seek_sequence, seek_time
 
 def sstf(requests, head):
@@ -21,5 +21,21 @@ def sstf(requests, head):
         seek_sequence.append(closest)
         remaining.remove(closest)
     
+    seek_time = sum(abs(seek_sequence[i] - seek_sequence[i+1]) for i in range(len(seek_sequence) - 1))
+    return seek_sequence, seek_time
+def scan(requests, head, max_cylinder):    """ SCAN Disk Scheduling (Elevator Algorithm) """
+    requests.append(head)
+    requests.sort()
+    direction = 1  # Move towards higher values
+    seek_sequence = []
+
+    if head in requests:
+        idx = requests.index(head)
+
+    if direction == 1:  # Moving right
+        seek_sequence.extend(requests[idx:])
+        seek_sequence.append(max_cylinder)  # Go to max limit
+        seek_sequence.extend(reversed(requests[:idx]))  # Then go back
+
     seek_time = sum(abs(seek_sequence[i] - seek_sequence[i+1]) for i in range(len(seek_sequence) - 1))
     return seek_sequence, seek_time
